bb7f9ebb5739f2416dfd23ee0b74559cea3451c5
matheuspc <matheus.pc@live.com>
2019-05-20 14:57:27 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(1212, 2500);
+            Console.WriteLine(ContaCorrente.TaxaOperacao);;
+        }
+    }
+}
